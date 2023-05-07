3d5ad90390eaadf83c96e178d5737591d48535c3
Chork Hieng <hiengchorknu@gmail.com>
2023-05-06 23:06:16 -0500

    change scripts.js -> app.js

--- scripts.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("aboutMeButton").addEventListener("click", function() {
-    document.getElementById("aboutMeText").innerHTML = "Hello, welcome to my simple website!"
-});
-  
-
-  
